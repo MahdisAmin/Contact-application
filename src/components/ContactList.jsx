ad44cfbae427a854a86430938b441d565785caf5
Mahdis Amin <mahdis.amin95@gmail.com>
2024-09-03 00:14:47 +0330

    background changed and cart color as well

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -10,34 +10,44 @@ function ContactList({
     <ul className="contact-item">
       {contacts.map((contact) => (
         // <li key={contact.id}>
-          <div className="contact-list">
-            <li className="user-avatar">
-              {/* <li key={contact.id}> */}
-              {contact.photo && (
-                <img
-                  className="avatar"
-                  src={contact.photo}
-                  alt={`${contact.firstName} ${contact.lastName}`}
-                />
-              )}
-              {/* </li> */}
-              {contact.firstName} {contact.lastName}
-            </li>
-            <li>📞{contact.phone}</li>
-            <li>📩{contact.email}</li>
+        <div className="contact-list">
+          <li className="user-avatar">
+            {/* <li key={contact.id}> */}
+            {contact.photo && (
+              <img
+                className="avatar"
+                src={contact.photo}
+                alt={`${contact.firstName} ${contact.lastName}`}
+              />
+            )}
+            {/* </li> */}
+            {contact.firstName} {contact.lastName}
+          </li>
+          <li>
+            {contact.phone}
+          </li>
+          <li>
+            {contact.email}
+          </li>
 
-            <div className="contact-actions">
-              <button
-                onClick={() => {
-                  setCurrentContact(contact);
-                  openEditModal();
-                }}
-              >
-                🖋
-              </button>
-              <button onClick={() => deleteContact(contact)}>🚮</button>
-            </div>
+          <div className="contact-actions">
+            <button
+              className="Edit-btn"
+              onClick={() => {
+                setCurrentContact(contact);
+                openEditModal();
+              }}
+            >
+              Edit
+            </button>
+            <button
+              className="delete-btn"
+              onClick={() => deleteContact(contact)}
+            >
+              Delete{" "}
+            </button>
           </div>
+        </div>
         // </li>
       ))}
     </ul>
